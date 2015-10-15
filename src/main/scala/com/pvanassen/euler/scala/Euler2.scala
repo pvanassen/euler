592aeba974654ee921cb59ed87be3ea706a8f776
Paul van Assen <source@pvanassen.nl>
2015-10-15 19:15:30 +0200

    Fixed #2

--- src/main/scala/com/pvanassen/euler/scala/Euler2.scala
+++ src/main/scala/com/pvanassen/euler/scala/Euler2.scala
@@ -6,10 +6,10 @@ package com.pvanassen.euler.scala
  *
  */
 class Euler2 extends EulerSolution {
-  val fibonacci: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibonacci.zip(fibonacci.tail).map {n => n._1 * n._2}
-  val problemNumber = 1
+  val fibonacci: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibonacci.zip(fibonacci.tail).map {n => n._1 + n._2}
+  val problemNumber = 2
 
   protected def execute(): String ={
-    return fibonacci.takeWhile(_ >= 4000000).filter(_ % 2 == 0).sum.toString
+    return fibonacci.takeWhile(_ < 4000000).filter(_ % 2 == 0).sum.toString
   }
 }
